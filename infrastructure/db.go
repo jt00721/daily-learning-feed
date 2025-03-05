a87fd26523120bcae5a2d4ad3b931a759c2a690c
jt00721 <titusgloverj@gmail.com>
2025-03-05 21:18:09 +0000

    Set up Postgres database connection and initialise AutoMigrate

---
+++ infrastructure/db.go
@@ -0,0 +1,41 @@
+package infrastructure
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/jt00721/daily-learning-feed/internal/entity"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_PORT"),
+	)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	err = DB.AutoMigrate(&entity.Resource{})
+	if err != nil {
+		log.Fatal("Migration failed:", err)
+	}
+	log.Println("Database initialized and migrated successfully")
+}
