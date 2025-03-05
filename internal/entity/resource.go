447d657392a22009ae21239ada4393351c5ef1bd
jt00721 <titusgloverj@gmail.com>
2025-03-05 21:18:57 +0000

    Define the resource model

---
+++ internal/entity/resource.go
@@ -0,0 +1,12 @@
+package entity
+
+import "time"
+
+type Resource struct {
+	ID        uint   `gorm:"primaryKey"`
+	Title     string `gorm:"not null"`
+	URL       string `gorm:"not null;unique"`
+	Category  string
+	Source    string
+	DateAdded time.Time `gorm:"autoCreateTime"`
+}
