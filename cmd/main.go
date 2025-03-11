019307daf9b8157879f2f104fcbc5290285471fb
jt00721 <titusgloverj@gmail.com>
2025-03-11 18:13:21 +0000

    Change main.go to simply initialise and start the app

--- cmd/main.go
+++ cmd/main.go
@@ -1,48 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"github.com/jt00721/daily-learning-feed/infrastructure"
-	"github.com/jt00721/daily-learning-feed/internal/handler"
-	"github.com/jt00721/daily-learning-feed/internal/repository"
+	"github.com/jt00721/daily-learning-feed/config"
+	"github.com/jt00721/daily-learning-feed/internal/routes"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// Initialize application
+	application := config.NewApp()
 
-	infrastructure.InitDB()
+	// Setup routes
+	routes.SetupRoutes(application.Router, application.ResourceRepo)
 
-	r := gin.Default()
-
-	repo := &repository.ResourceRepository{DB: infrastructure.DB}
-	rssFetcher := infrastructure.NewRSSFetcher()
-	rssHandler := &handler.RSSHandler{Fetcher: rssFetcher, Repo: repo}
-	resourceHandler := &handler.ResourceHandler{Repo: repo}
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Welcome to the Daily Learning Feed!"})
-	})
-
-	r.POST("/resources", resourceHandler.CreateResource)
-	r.GET("/resources", resourceHandler.GetResources)
-	r.GET("/resources/:id", resourceHandler.GetResourceByID)
-	r.PUT("/resources/:id", resourceHandler.UpdateResource)
-	r.DELETE("/resources/:id", resourceHandler.DeleteResource)
-
-	r.GET("/fetch-rss", rssHandler.FetchAndStoreResources)
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	fmt.Println("Server running on port", port)
-	r.Run(":" + port)
+	// Run application
+	application.Run()
 }
