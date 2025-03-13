5e717f4c019c22b8df95f70ec174f843711a7f51
jt00721 <titusgloverj@gmail.com>
2025-03-13 14:54:28 +0000

    Fix environment variable loading for Railway Postgres

--- infrastructure/db.go
+++ infrastructure/db.go
@@ -1,7 +1,6 @@
 package infrastructure
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -19,14 +18,10 @@ func InitDB() {
 		log.Println("Warning: .env file not found, using Railway environment variables")
 	}
 
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_DB"),
-		os.Getenv("POSTGRES_PORT"),
-	)
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
